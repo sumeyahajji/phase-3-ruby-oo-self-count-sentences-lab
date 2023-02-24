d58a0d633fc4544c41fda08fc7e5513b5ac1c4b6
+ <+>
2023-02-24 13:37:10 +0300

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
 require 'pry'
 
 class String
-
   def sentence?
-    
+    self.end_with?('.')
   end
-
+  
   def question?
-
+    self.end_with?('?')
   end
-
+  
   def exclamation?
-
+    self.end_with?('!')
   end
-
+  
   def count_sentences
-
+    sentences = self.split(/[.?!]/)
+    sentences.reject! { |s| s.empty? }
+    sentences.length
   end
 end
